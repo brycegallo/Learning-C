14483da0d0ecb7067affd85cefa702f3fcaa5c09
bryce <76966016+brycegallo@users.noreply.github.com>
2025-07-02 15:37:31 -0600

    add new program

---
+++ files.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+	printf("Usage: %s <filename>\n", argv[0]);
+	return 0;
+    }
+    
+    // ORing bitwise flags combines them, e.g. O_RDWR and O_CREAT here
+    int fd = open(argv[1], O_RDWR | O_CREAT, 0644);
+    if (fd == -1) {
+	perror("open");
+	return -1;
+    }
+
+    char *mydata = "hello there\n";
+    write(fd, mydata, strlen(mydata));
+
+    close(fd);
+    return 0;
+}
